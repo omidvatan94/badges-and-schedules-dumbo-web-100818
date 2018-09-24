140cdd17dbf92b0c0266089e5ba02ad2d444ab63
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-24 19:59:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,12 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    badge_maker(name)
+def batch_badge_creator(speakers)
+  # returns an array of badge messages, using badge_maker
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
   end
+  badge_messages
+end
